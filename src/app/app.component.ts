057def8d885d119f23ea33c6746fe4e3638aefe0
Rashmi Bora <you@example.com>
2022-06-30 23:12:51 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngforngif';
+  title = 'Demo';
+  showMe = false;
+  showMe1 = false;
+  onToggle(){
+     this.showMe= !this.showMe; 
+     this.showMe=true;
+     this.showMe1=false;
+  }
+  onToggle1(){
+    this.showMe1= !this.showMe1;
+    this.showMe1=true;
+    this.showMe=false;
+ }
+  veg=[
+    {name:'potato',image:'./assets/patota.jpg'},
+    {name:'capsicum',image:'./assets/capsicum.jpg'},
+    {name:'onion',image:'./assets/onion.jpg'},
+    {name:'cucumber',image:'./assets/cucumber.jpg'}
+  ];
+  fruits=[
+    {name:'apple',image:'./assets/apple.jpg'},
+    {name:'watermelon',image:'./assets/watermelon.jpg'},
+    {name:'tomato',image:'./assets/tomato.jpg'}
+  ];
 }
